f330cd996045410387296396d44a39d1c904efa8
Amine RAJI <molntek0@gmail.com>
2025-04-04 10:27:02 +0200

    Fixed namespace dep on external-dns

--- modules/external-dns/main.tf
+++ modules/external-dns/main.tf
@@ -1,11 +1,17 @@
+resource "kubernetes_namespace" "external_dns" {
+  metadata {
+    name = "external-dns"
+  }
+}
+
 resource "helm_release" "external-dns" {
   name       = "external-dns"
   chart      = "external-dns"
   repository = "https://kubernetes-sigs.github.io/external-dns"
   version    = "1.15.2"
-  namespace  = "external-dns"
+  namespace = kubernetes_namespace.external_dns.metadata[0].name
+  create_namespace = false
 
-  create_namespace = true
 
   depends_on = [kubernetes_secret.cloudflare_api_token]
 
@@ -18,7 +24,7 @@ resource "helm_release" "external-dns" {
 resource "kubernetes_secret" "cloudflare_api_token" {
   metadata {
     name      = "cloudflare-api-key"
-    namespace = "external-dns"
+    namespace = kubernetes_namespace.external_dns.metadata[0].name
   }
 
   data = {
