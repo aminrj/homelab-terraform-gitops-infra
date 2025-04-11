6f78dade5e208994a7172f5e1a055e19225d0fce
Amine RAJI <molntek0@gmail.com>
2025-04-11 20:20:04 +0200

    Updated external-secret module

--- modules/external-secrets/main.tf
+++ modules/external-secrets/main.tf
@@ -7,9 +7,33 @@ resource "helm_release" "external_secrets" {
   version          = "0.15.1"
 
   values = [
-    <<-EOT
-      installCRDs: true
-    EOT
-  ]
+    yamlencode({
+      installCRDs : true
 
+      # Required to process ClusterSecretStores across namespaces
+      processClusterStore        : true
+      processClusterExternalSecret : true
+
+      # Full cluster RBAC permissions to read ClusterSecretStores and secrets across namespaces
+      rbac : {
+        create : true
+        clusterWide : true
+      }
+
+      serviceAccount : {
+        create : true
+        name   : "external-secrets-sa"
+      }
+
+      controllerClass : "" # optional, only set this if you want multiple ESO controllers
+      
+      webhook : {
+        create : true
+      }
+
+      certController : {
+        create : true
+      }
+    })
+  ]
 }
